a73418d3380e3a29c28587116c0988a5c14e35de
David <davidcerrudo@gmail.com>
2024-10-31 11:43:23 +0100

    mejorando upgrades fallos 2

--- app/src/main/java/com/example/pumpkinclicker/ui/UpgradeScreen.kt
+++ app/src/main/java/com/example/pumpkinclicker/ui/UpgradeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -17,9 +18,9 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 @SuppressLint("UnrememberedMutableState")
 @Composable
 fun UpgradeScreen(gameViewModel: GameViewModel = viewModel()) {
-    var cantM1 = 1;
-    var cantM2 = 1;
-    var clickcant = 1;
+    val clickcant = remember { mutableStateOf(1) }
+    val cantM1 = remember { mutableStateOf(1) }
+    val cantM2 = remember { mutableStateOf(1) }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -31,34 +32,34 @@ fun UpgradeScreen(gameViewModel: GameViewModel = viewModel()) {
             color = Color(0xFFFFA500),
             modifier = Modifier.padding(bottom = 24.dp)
         )
-        Button(onClick = {gameViewModel.buyClickUpgrade(cost = 10 * clickcant)
-            clickcant++},
+        Button(onClick = {gameViewModel.buyClickUpgrade(cost = 10 * clickcant.value)
+            clickcant.value++},
 
                 modifier = Modifier
                 .fillMaxWidth(0.7f)
                 .padding(8.dp)
 
         ){
-            Text(text = "Cuchillo: +1 Caramelo/s por click(Coste: ${clickcant * 10} Caramelos) $clickcant", fontSize = 16.sp)
+            Text(text = "Cuchillo: +1 Caramelo/s por click(Coste: ${clickcant.value * 10} Caramelos) ${clickcant.value}", fontSize = 16.sp)
         }
         Button(
-            onClick = { gameViewModel.buyUpgrade(cost = (50 * cantM1), additionalPassivePoints = 1)
-                      cantM1++},
+            onClick = { gameViewModel.buyUpgrade(cost = 50 * cantM1.value, additionalPassivePoints = 1)
+                      cantM1.value++},
             modifier = Modifier
                 .fillMaxWidth(0.7f)
                 .padding(8.dp)
         ) {
-            Text(text = "Mejora: +1 Caramelo/s (Coste: ${cantM1*50} Caramelos)$cantM1", fontSize = 16.sp)
+            Text(text = "Mejora: +1 Caramelo/s (Coste: ${cantM1.value*50} Caramelos)${cantM1.value}", fontSize = 16.sp)
         }
 
         Button(
-            onClick = { gameViewModel.buyUpgrade(cost = (100 * cantM2), additionalPassivePoints = 2)
-                      cantM2++},
+            onClick = { gameViewModel.buyUpgrade(cost = 100 * cantM2.value, additionalPassivePoints = 2)
+                      cantM2.value++},
             modifier = Modifier
                 .fillMaxWidth(0.7f)
                 .padding(8.dp)
         ) {
-            Text(text = "Mejora: +2 Caramelos/s (Coste: ${cantM2 *100} Caramelos)$cantM2", fontSize = 16.sp)
+            Text(text = "Mejora: +2 Caramelos/s (Coste: ${cantM2.value *100} Caramelos)${cantM2.value}", fontSize = 16.sp)
         }
     }
 }
